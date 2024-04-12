ef92a2306222592560eecf512fa97467c65f2f9d
victor-arana-kenility <victor.arana@kenility.com>
2024-04-12 12:36:53 -0400

    publish version 0.0.4

--- Package.swift
+++ Package.swift
@@ -13,12 +13,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TruvideoSdk",
-            url: "https://github.com/victor-arana-kenility/truvideo-sdk-ios-core-test/releases/download/0.0.3/TruvideoSdk.xcframework.zip",
-            checksum: "6eec9674b429c27ad255d6d7fb8cd96e1ec844ce79e1984111e85b4bd9d4e605"
+            url: "https://github.com/victor-arana-kenility/truvideo-sdk-ios-core-test/releases/download/0.0.4/TruvideoSdk.xcframework.zip",
+            checksum: "2e86827050af07096798ad37442562723e8da33ff98f1e68d907709d8c90fb87"
         ),
         .binaryTarget(
             name: "Common",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-common/releases/download/0.0.26/Common.xcframework.zip",
+            url: "https://github.com/victor-arana-kenility/truvideo-sdk-ios-common/releases/download/0.0.1/Common.xcframework.zip",
             checksum: "43fcac1b0801d7c3f06ba256be78b133f3317e16d96bbd84ec0b110b59448c76"
         ),
         .binaryTarget(
